2eee314cf3740b8d2b4d0dd1e6fa5b186df81a66
DrRon <DoctorRon64@users.noreply.github.com>
2026-07-02 11:50:47 +0200

    Fixing templatescene and bullet

---
+++ Samples~/Cuphead Example/Scripts/Game/Projectiles/Bullet.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using UnityEngine;
+using Utility;
+
+namespace Game {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bullet : MonoBehaviour, IPoolable {
+        [SerializeField] private int m_damageValue = 1;
+        [SerializeField] private DamagableTeam m_ownerTeam = DamagableTeam.Opponent;
+        [SerializeField] private LayerMask m_enviourmentMask;
+        [SerializeField] private SpriteRenderer m_spriteRenderer;
+        [SerializeField] private Animator m_anim;
+        
+        [SerializeField] private string idleState = "bullet_idle";
+        [SerializeField] private string explodeState = "bullet_explode";
+
+        public bool active { get; set; }
+        private Rigidbody2D m_rb;
+        private ObjectPool<Bullet> m_objectPool;
+        private bool m_isDestroying;
+
+        public bool isDestroying => m_isDestroying;
+
+        private void Awake() {
+            m_rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void SetupBullet(ObjectPool<Bullet> _pool) {
+            m_objectPool = _pool;
+        }
+
+        public void SetDirection(Vector2 _direction, float _speed) {
+            m_rb.linearVelocity = _direction.normalized * _speed;
+        }
+
+        public void SetOwnerTeam(DamagableTeam _team) {
+            m_ownerTeam = _team;
+        }
+
+        public void SetRotation(Vector2 _direction) {
+            float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        private void OnTriggerEnter2D(Collider2D _other) {
+            if (m_isDestroying) return;
+
+            if (_other.TryGetComponent(out IDamagable damagable)) {
+                if (damagable.team == m_ownerTeam)
+                    return;
+
+                damagable.TakeDamage(m_damageValue);
+                DestroyBullet();
+                return;
+            }
+
+            bool hitEnvironment = (m_enviourmentMask.value & (1 << _other.gameObject.layer)) != 0;
+            if (hitEnvironment) {
+                DestroyBullet();
+            }
+        }
+
+        private IEnumerator WaitUntilAnimationFinished() {
+            if (m_anim == null) {
+                CleanupBullet();
+                yield break;
+            }
+
+            yield return null;
+            while (m_anim != null) {
+                AnimatorStateInfo state = m_anim.GetCurrentAnimatorStateInfo(0);
+                if (!state.IsName(explodeState) || state.normalizedTime >= 1f)
+                    break;
+                yield return null;
+            }
+
+            CleanupBullet();
+        }
+
+        public void EnablePoolable() {
+            m_isDestroying = false;
+            gameObject.SetActive(true);
+            if (m_spriteRenderer != null)
+                m_spriteRenderer.enabled = true;
+            if (m_anim == null) return;
+            m_anim.enabled = true;
+            m_anim.Rebind();
+            m_anim.Update(0f);
+            if (m_anim.HasState(0, Animator.StringToHash(idleState)))
+                m_anim.Play(idleState, 0, 0f);
+        }
+
+        public void DisablePoolable() {
+            StopAllCoroutines();
+            m_isDestroying = false;
+            m_rb.linearVelocity = Vector2.zero;
+            gameObject.SetActive(false);
+            if (m_spriteRenderer != null)
+                m_spriteRenderer.enabled = true;
+        }
+
+        public void SetPosition(Vector2 _position) {
+            transform.position = _position;
+        }
+
+        private void DestroyBullet() {
+            if (m_isDestroying) return;
+            m_isDestroying = true;
+            m_rb.linearVelocity = Vector2.zero;
+            if (m_anim == null) {
+                CleanupBullet();
+                return;
+            }
+
+            m_anim.Play(explodeState, 0, 0f);
+            StartCoroutine(WaitUntilAnimationFinished());
+        }
+
+        private void CleanupBullet() {
+            if (m_objectPool != null) {
+                m_objectPool.DeactivateItem(this);
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+
+        public void BreakBullet() {
+            DestroyBullet();
+        }
+    }
+}
